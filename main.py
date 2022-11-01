643b05f3651821db0ddde64e2198faa24f02eed7
Mike Gale <mgale01@gmail.com>
2022-11-01 14:56:25 -0500

    folder watcher example working

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+if __name__ == "__main__":
+    patterns = ["*"]
+    ignore_patterns = None
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+
+def on_created(event):
+    print(f"hey, {event.src_path} has been created!")
+
+
+def on_deleted(event):
+    print(f"what the f**k! Someone deleted {event.src_path}!")
+
+
+def on_modified(event):
+    print(f"hey buddy, {event.src_path} has been modified")
+
+
+def on_moved(event):
+    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+
+
+my_event_handler.on_created = on_created
+my_event_handler.on_deleted = on_deleted
+my_event_handler.on_modified = on_modified
+my_event_handler.on_moved = on_moved
+
+path = "./input"
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except:
+    KeyboardInterrupt
+
+my_observer.stop()
+my_observer.join()
